c180d87d973985f204d1160c5e03f6deac4dec96
Uday <iamuday1618@gmail.com>
2020-09-25 00:19:57 +0530

    Bug fixes
    Signed-off-by: Uday <iamuday1618@gmail.com>

--- minesweeper.py
+++ minesweeper.py
@@ -179,7 +179,8 @@ class MinesweeperAI:
                     continue
 
                 if 0 <= i < self.height and 0 <= j < self.width:
-                    neighbors.add((i, j))
+                    if (i, j) not in self.moves_made:
+                        neighbors.add((i, j))
 
         return neighbors
 
